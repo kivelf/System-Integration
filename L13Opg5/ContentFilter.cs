6b62d861bd29cfb56d77891645ac2aff53ebe9e3
Remy <rumena.mp@gmail.com>
2024-10-16 15:30:24 +0200

    L13Opg5 - using Content Filter pattern

---
+++ L13Opg5/ContentFilter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace L13Opg5
+{
+    internal class ContentFilter
+    {
+        protected MessageQueue cargoQueue;
+
+        public ContentFilter(MessageQueue cargoQueue)
+        { 
+            this.cargoQueue = cargoQueue;
+        }
+
+        public void OnMessage(Message message)
+        {
+            XmlDocument xml = new XmlDocument();
+            string XMLDocument;
+            using (Stream body = message.BodyStream)
+            using (StreamReader reader = new StreamReader(body))
+            {
+                XMLDocument = reader.ReadToEnd();
+            }
+
+            xml.LoadXml(XMLDocument);
+            XmlNode flightDetails = xml.SelectSingleNode("/FlightDetailsInfoResponse");
+
+            double total = 0;
+
+
+            if (flightDetails != null)
+            {
+                XmlNode flight = flightDetails.SelectSingleNode("Flight");
+                XmlNode passenger = flightDetails.SelectSingleNode("Passenger");
+                XmlNodeList luggageList = flightDetails.SelectNodes("Luggage");
+
+                foreach (XmlNode node in luggageList) 
+                { 
+                    string weight = node.SelectSingleNode("Weight").InnerText;
+                    total += Convert.ToDouble(weight);
+                }
+
+                SendCargoMessage(flight, passenger, total);
+
+                Console.WriteLine($"Message {message.Label} total weight calculated and sent to Cargo queue.");
+            }
+        }
+
+        private void SendCargoMessage(XmlNode flight, XmlNode passenger, double totalWeight)
+        {
+            XmlDocument cargoDoc = new XmlDocument();
+            XmlElement cargoRoot = cargoDoc.CreateElement("CargoInfo");
+            cargoDoc.AppendChild(cargoRoot);
+            cargoRoot.AppendChild(cargoDoc.ImportNode(flight, true));
+            cargoRoot.AppendChild(cargoDoc.ImportNode(passenger, true));
+
+            Message msg = new Message
+            {
+                Body = cargoDoc,
+                Label = $"Total weight: {totalWeight}",
+                Formatter = new XmlMessageFormatter(new Type[] { typeof(XmlDocument) })
+            };
+
+            Console.WriteLine($"Sending message: {msg.Label}");
+            cargoQueue.Send(msg);
+        }
+    }
+}
---
+++ L13Opg5/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace L13Opg5
+{
+    // we need to use a Content Filter: removes unimportant data 
+    // items from a message, leaving only important items
+    internal class Program
+    {
+        private static MessageQueue checkInQueue;
+        private static MessageQueue cargoQueue;
+        private static ContentFilter contentFilter;
+        static void Main(string[] args)
+        {
+            // creating the two queues
+            if (!MessageQueue.Exists(@".\Private$\L13AirportCheckInOutput"))
+            {
+                MessageQueue.Create(@".\Private$\L13AirportCheckInOutput");
+            }
+            checkInQueue = new MessageQueue(@".\Private$\L13AirportCheckInOutput");
+            checkInQueue.Label = "CheckIn Queue";
+
+            if (!MessageQueue.Exists(@".\Private$\L13AirportCargo"))
+            {
+                MessageQueue.Create(@".\Private$\L13AirportCargo");
+            }
+            cargoQueue = new MessageQueue(@".\Private$\L13AirportCargo");
+            cargoQueue.Label = "Passenger Queue";
+
+            // creating the content filter
+            contentFilter = new ContentFilter(cargoQueue);
+
+            // sending the check-in message
+            SendCheckInMessage();
+
+            Console.WriteLine("Listening for messages...");
+            while (true) { }
+        }
+
+        // sending the XML message containing the check-in info
+        private static void SendCheckInMessage()
+        {
+            XElement CheckInFile = XElement.Load(@"CheckedInPassenger.xml");
+            Console.WriteLine("Original message:");
+            Console.WriteLine(CheckInFile);
+            Console.WriteLine("------------------------------");
+            string AirlineCompany = "SAS";
+            checkInQueue.Send(CheckInFile, AirlineCompany);
+
+            // listening for incoming messages from the CheckInOutput queue
+            checkInQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(OnMessageReceived);
+            checkInQueue.BeginReceive();
+        }
+
+        private static void OnMessageReceived(object sender, ReceiveCompletedEventArgs e)
+        {
+            MessageQueue mq = (MessageQueue)sender;
+            Message receivedMsg = mq.EndReceive(e.AsyncResult);
+
+            // use the existing content filer instance to process the received message
+            contentFilter.OnMessage(receivedMsg);
+
+            // start listening for the next message
+            mq.BeginReceive();
+        }
+    }
+}
