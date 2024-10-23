b7bc6a3c19e08607f8ef0e0535cd1ab3ea7c9d9e
Remy <rumena.mp@gmail.com>
2024-10-23 11:01:39 +0200

    L14Opg1 - Recipient List mønsteret

---
+++ L14Opg1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace L14Opg1
+{
+    internal class Program
+    {
+        private static MessageQueue customsAndBorderProtectionQueue;
+        private static MessageQueue USCitizenQueue;
+        private static MessageQueue DKCitizenQueue;
+        private static MessageQueue JPCitizenQueue;
+        private static RecipientList recipientList;
+        static void Main(string[] args)
+        {
+            // creating the four queues
+            if (!MessageQueue.Exists(@".\Private$\L14CustomsAndBorderProtectionQueue"))
+            {
+                MessageQueue.Create(@".\Private$\L14CustomsAndBorderProtectionQueue");
+            }
+            customsAndBorderProtectionQueue = new MessageQueue(@".\Private$\L14CustomsAndBorderProtectionQueue");
+            customsAndBorderProtectionQueue.Label = "Customs & Border Protection Queue";
+
+            if (!MessageQueue.Exists(@".\Private$\L14USQueue"))
+            {
+                MessageQueue.Create(@".\Private$\L14USQueue");
+            }
+            USCitizenQueue = new MessageQueue(@".\Private$\L14USQueue");
+            USCitizenQueue.Label = "US Citizen Queue";
+
+            if (!MessageQueue.Exists(@".\Private$\L14DKQueue"))
+            {
+                MessageQueue.Create(@".\Private$\L14DKQueue");
+            }
+            DKCitizenQueue = new MessageQueue(@".\Private$\L14DKQueue");
+            DKCitizenQueue.Label = "DK Citizen Queue";
+
+            if (!MessageQueue.Exists(@".\Private$\L14JPQueue"))
+            {
+                MessageQueue.Create(@".\Private$\L14JPQueue");
+            }
+            JPCitizenQueue = new MessageQueue(@".\Private$\L14JPQueue");
+            JPCitizenQueue.Label = "JP Citizen Queue";
+
+            // creating the recipient list
+            recipientList = new RecipientList();
+            recipientList.AddRecipientChannel(USCitizenQueue);
+            recipientList.AddRecipientChannel(DKCitizenQueue);
+            recipientList.AddRecipientChannel(JPCitizenQueue);
+
+            // sending the passenger messages
+            SendPassengerMessages();
+
+            Console.WriteLine("Listening for messages...");
+            while (true) { }
+        }
+
+        // sending the XML message containing the check-in info
+        private static void SendPassengerMessages()
+        {
+            XElement PassengerFile = XElement.Load(@"Passenger1.xml");
+            Console.WriteLine("Original message:");
+            Console.WriteLine(PassengerFile);
+            Console.WriteLine("------------------------------");
+            customsAndBorderProtectionQueue.Send(PassengerFile, "Passenger 1");
+
+            PassengerFile = XElement.Load(@"Passenger2.xml");
+            Console.WriteLine("Original message:");
+            Console.WriteLine(PassengerFile);
+            Console.WriteLine("------------------------------");
+            customsAndBorderProtectionQueue.Send(PassengerFile, "Passenger 2");
+
+            // listening for incoming messages from the Customs & Border Protection queue
+            customsAndBorderProtectionQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(OnMessageReceived);
+            customsAndBorderProtectionQueue.BeginReceive();
+        }
+
+        private static void OnMessageReceived(object sender, ReceiveCompletedEventArgs e)
+        {
+            MessageQueue mq = (MessageQueue)sender;
+            Message receivedMsg = mq.EndReceive(e.AsyncResult);
+
+            // use the existing recipient list instance to process the received message
+            recipientList.OnMessage(receivedMsg);
+
+            // start listening for the next message
+            mq.BeginReceive();
+        }
+    }
+}
---
+++ L14Opg1/RecipientList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace L14Opg1
+{
+    internal class RecipientList
+    {
+        Dictionary<string, MessageQueue> recipients = new Dictionary<string, MessageQueue>();
+        public RecipientList() { }
+
+        public void AddRecipientChannel(MessageQueue recipient) 
+        {
+            string country = recipient.Label.Substring(0, 2);
+            recipients.Add(country, recipient);
+        }
+
+        public void OnMessage(Message message)
+        {
+            XmlDocument xml = new XmlDocument();
+            string XMLDocument;
+            using (Stream body = message.BodyStream)
+            using (StreamReader reader = new StreamReader(body))
+            {
+                XMLDocument = reader.ReadToEnd();
+            }
+
+            xml.LoadXml(XMLDocument);
+            XmlNode passengerDetails = xml.SelectSingleNode("/CBPArrivalInfo");
+
+            if (passengerDetails != null)
+            {
+                XmlNodeList passInfo = passengerDetails.SelectNodes("Passport");
+
+                foreach (XmlNode node in passInfo)
+                {
+                    string nationality = node.SelectSingleNode("Nationality").InnerText;
+                    bool found = false;
+                    while (!found) 
+                    {
+                        foreach (KeyValuePair<string, MessageQueue> entry in recipients) 
+                        {
+                            if (entry.Key == nationality) 
+                            { 
+                                found = true;
+                                SendMessage(entry.Value, xml);
+                            }
+                        }
+                        // ending the loop
+                        found = true;
+                    }
+                }
+            }
+        }
+
+        private void SendMessage(MessageQueue receiver, XmlDocument message)
+        {
+            Message msg = new Message
+            {
+                Body = message,
+                Label = "From Bluff City International Airport Customs & Border Protection",
+                Formatter = new XmlMessageFormatter(new Type[] { typeof(XmlDocument) })
+            };
+
+            Console.WriteLine($"Message sent to {receiver.Label}");
+            receiver.Send(msg);
+        }
+    }
+}
