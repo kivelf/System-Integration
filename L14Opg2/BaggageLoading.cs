c6163386f964059816bb4b43f0b6efbdcf8b3145
Remy <rumena.mp@gmail.com>
2024-10-26 11:44:41 +0200

    L14Opg2 solved using Dynamic Router pattern

---
+++ L14Opg2/BaggageLoading.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace L14Opg2
+{
+    internal class BaggageLoading
+    {
+        protected MessageQueue inQueue;
+        protected MessageQueue controlQueue;
+        private Dictionary<string, string> dynamicRules = new Dictionary<string, string>();
+
+        public BaggageLoading(MessageQueue inQueue, MessageQueue controlQueue) 
+        { 
+            this.inQueue = inQueue;
+            this.controlQueue = controlQueue;
+            inQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(OnMessageInQueue);
+            controlQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(OnMessageControlQueue);
+            inQueue.BeginReceive();
+            controlQueue.BeginReceive();
+        }
+
+        private async void OnMessageInQueue(Object source, ReceiveCompletedEventArgs asyncResult) 
+        { 
+            MessageQueue mq = (MessageQueue)source;
+            Message msg = mq.EndReceive(asyncResult.AsyncResult);
+            string lbl = msg.Label;
+            XmlDocument xml = new XmlDocument();
+            string XMLDocument = null;
+
+            Stream body = msg.BodyStream;
+            using (StreamReader reader = new StreamReader(body)) 
+            { 
+                XMLDocument = await reader.ReadToEndAsync();
+            }
+            xml.LoadXml(XMLDocument);
+
+            string gateNr = xml.SelectSingleNode("/BaggageTransaction/GateNumber")?.InnerText;
+            if (gateNr == null) 
+            {
+                Console.WriteLine("Gate number could not be retrieved!");
+            }
+
+            if (dynamicRules.ContainsKey(gateNr)) 
+            { 
+                SendMessage(dynamicRules[gateNr], xml);
+            }
+
+            mq.BeginReceive();
+        }
+
+        private async void OnMessageControlQueue(Object source, ReceiveCompletedEventArgs asyncResult)
+        {
+            MessageQueue mq = (MessageQueue)source;
+            Message msg = mq.EndReceive(asyncResult.AsyncResult);
+            string lbl = msg.Label;
+            string messageContent = (string)msg.Body;
+            string[] controlRules = messageContent.Split(new char[] { ',' });
+
+            string gateNr = controlRules[0];
+            string queuePath = controlRules[1];
+
+            if (dynamicRules.ContainsKey(gateNr))
+            {
+                dynamicRules[gateNr] = queuePath;
+            }
+            else
+            { 
+                dynamicRules.Add(gateNr, queuePath);
+            }
+            mq.BeginReceive();
+        }
+
+        private void SendMessage(string path, XmlDocument xml) 
+        {
+            MessageQueue mq = null;
+            string queuePath = $@".\Private$\L14{path}";
+            Console.WriteLine("Baggage Loading Department sending message to " + queuePath);
+
+            if (!MessageQueue.Exists(queuePath))
+            {
+                MessageQueue.Create(queuePath);
+            }
+            mq = new MessageQueue(queuePath);
+
+            mq.Send(xml);
+        }
+    }
+}
---
+++ L14Opg2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace L14Opg2
+{
+    internal class Program
+    {
+        // using Dynamic Router pattern
+        private static MessageQueue inQueue;
+        private static MessageQueue controlQueue;
+        private static BaggageLoading baggageLoading;
+        static void Main(string[] args)
+        {
+            // creating the two queues
+            if (!MessageQueue.Exists(@".\Private$\L14inQueue"))
+            {
+                MessageQueue.Create(@".\Private$\L14inQueue");
+            }
+            inQueue = new MessageQueue(@".\Private$\L14inQueue");
+            inQueue.Label = "In Queue";
+
+            if (!MessageQueue.Exists(@".\Private$\L14controlQueue"))
+            {
+                MessageQueue.Create(@".\Private$\L14controlQueue");
+            }
+            controlQueue = new MessageQueue(@".\Private$\L14controlQueue");
+            controlQueue.Label = "Control Queue";
+
+            // creating the baggage loading department
+            baggageLoading = new BaggageLoading(inQueue, controlQueue);
+
+            SASProgram(controlQueue);
+            SWProgram(controlQueue);
+
+            XElement baggage1 = XElement.Load(@"Baggagetransaction1.xml");
+            XElement baggage2 = XElement.Load(@"Baggagetransaction2.xml");
+            XElement baggage3 = XElement.Load(@"Baggagetransaction3.xml");
+
+            inQueue.Send(baggage1);
+            Console.WriteLine("Baggage sent to Gate 14");
+
+            inQueue.Send(baggage2);
+            Console.WriteLine("Baggage sent to Gate 3");
+
+            inQueue.Send(baggage3);
+            Console.WriteLine("Baggage sent to Gate 14");
+
+            while (true) { }
+        }
+
+        static void SASProgram(MessageQueue controlQueue) 
+        {
+            Console.WriteLine("SAS requests all baggages for Gate 3");
+
+            if (!MessageQueue.Exists(@".\Private$\L14SASQueue"))
+            {
+                MessageQueue.Create(@".\Private$\L14SASQueue");
+            }
+
+            controlQueue.Send("3,SAS");
+        }
+
+        static void SWProgram(MessageQueue controlQueue)
+        {
+            Console.WriteLine("SW requests all baggages for Gate 14");
+
+            if (!MessageQueue.Exists(@".\Private$\L14SWQueue"))
+            {
+                MessageQueue.Create(@".\Private$\L14SWQueue");
+            }
+
+            controlQueue.Send("14,SW");
+        }
+    }
+}
