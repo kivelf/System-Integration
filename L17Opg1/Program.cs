7019ab0572697b9ad24e9bf76f716cd0906f4971
Remy <rumena.mp@gmail.com>
2024-11-06 10:24:29 +0100

    weather API connection test program

---
+++ L17Opg1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace L17Opg1
+{
+    // weather API test
+    internal class Program
+    {
+        // Enter your API key here.
+        private const string API_KEY = "8c0a1499aadee9bcc9e8df577ac4faf8";
+
+        // Query URLs. Replace @LOC@ with the location.
+        private const string CurrentUrl =
+            "http://api.openweathermap.org/data/2.5/weather?" +
+            "q=@LOC@&mode=xml&units=metric&APPID=" + API_KEY;
+        private const string ForecastUrl =
+            "http://api.openweathermap.org/data/2.5/forecast?" +
+            "q=@LOC@&mode=xml&units=metric&APPID=" + API_KEY;
+
+        static void Main(string[] args)
+        {
+            string url = CurrentUrl.Replace("@LOC@", "London");
+            Console.WriteLine(GetFormattedXml(url));
+            Console.ReadLine();
+        }
+
+        // Return the XML result of the URL.
+        private static string GetFormattedXml(string url)
+        {
+            // Create a web client.
+            using (WebClient client = new WebClient())
+            {
+                // Get the response string from the URL.
+                string xml = client.DownloadString(url);
+
+                // Load the response into an XML document.
+                XmlDocument xml_document = new XmlDocument();
+                xml_document.LoadXml(xml);
+
+                // Format the XML.
+                using (StringWriter string_writer = new StringWriter())
+                {
+                    XmlTextWriter xml_text_writer =
+                        new XmlTextWriter(string_writer);
+                    xml_text_writer.Formatting = Formatting.Indented;
+                    xml_document.WriteTo(xml_text_writer);
+
+                    // Return the result.
+                    return string_writer.ToString();
+                }
+            }
+        }
+    }
+}
