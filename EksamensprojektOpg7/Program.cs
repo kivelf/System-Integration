832f2a442056e39b314becc93ca2101438b0a3bc
Remy <rumena.mp@gmail.com>
2025-01-10 10:57:16 +0100

    Opgave 7 fra mit eksamensprojekt (4.sem).

---
+++ EksamensprojektOpg7/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EksamensprojektOpg7
+{
+    internal class Program
+    {
+        // Opgave 7
+        // Implementerer oversættelsen (translation) mellem systemerne Medlem og Nyhedsbrev.
+
+        // queues and objects representing our systems
+        private static MessageQueue outQueue;   // beskeder fra Medlemsregisteret bliver først sendt her
+        private static MessageQueue translatedMessageQueue; // translated messages som går til Nyhedsbrevsystemet
+        private static Medlemsregister medlemsregister;
+        private static Nyhedsbrev nyhedsbrev;
+        private static Translator translator;
+        private static List<Medlem> medlemmer = new List<Medlem>();
+
+        static void Main(string[] args)
+        {
+            // opretter queues
+            CreateQueues();
+
+            // opretter objekter, som repræsenterer vores systemer
+            medlemsregister = new Medlemsregister(outQueue);
+            nyhedsbrev = new Nyhedsbrev(translatedMessageQueue);
+            translator = new Translator(outQueue, translatedMessageQueue);
+
+            // opretter nogle medlemmer (obs, at der bliver oprettet 5 medlemmer, men kun 3 er tilmeldt nyhedsbrevet!)
+            CreateMembers();
+            foreach (var member in medlemmer)
+            {
+                medlemsregister.AddMedlem(member);
+            }
+
+            medlemsregister.PrintMedlemmer();
+            Console.WriteLine("Retrieving newsletter subscriber list, please wait...");
+            Thread.Sleep(3000);
+            nyhedsbrev.PrintNewsletterSubscribers();
+
+            while (true) { }
+        }
+
+        private static void CreateQueues()
+        {
+            if (!MessageQueue.Exists(@".\Private$\L21OutQueue"))
+            {
+                MessageQueue.Create(@".\Private$\L21OutQueue");
+            }
+            outQueue = new MessageQueue(@".\Private$\L21OutQueue");
+            outQueue.Label = "Out Queue";
+
+            if (!MessageQueue.Exists(@".\Private$\L21TranslatedMessageQueue"))
+            {
+                MessageQueue.Create(@".\Private$\L21TranslatedMessageQueue");
+            }
+            translatedMessageQueue = new MessageQueue(@".\Private$\L21TranslatedMessageQueue");
+            translatedMessageQueue.Label = "Medlemsregister Queue";
+        }
+
+        private static void CreateMembers()
+        {
+            Medlem medlem1 = new Medlem
+            {
+                MedlemsNummer = 12345,
+                Fornavn = "John",
+                Efternavn = "Doe",
+                Adresselinie1 = "Nygade 123",
+                Adresselinie2 = null,
+                PostNr = "2200",
+                ByNavn = "København N",
+                MailAdresse = "john@doe.dk",
+                ErTilmeldtNyhedsbrev = true,
+                DatoBrev = "23. oktober 2024"
+            };
+
+            Medlem medlem2 = new Medlem
+            {
+                MedlemsNummer = 22334,
+                Fornavn = "Bob",
+                Efternavn = "Bobsson",
+                Adresselinie1 = "Gamlegade 321",
+                Adresselinie2 = "st.th.",
+                PostNr = "5000",
+                ByNavn = "Odense C",
+                MailAdresse = "bob@thebob.dk",
+                ErTilmeldtNyhedsbrev = true,
+                DatoBrev = "29. november 2024"
+            };
+
+            Medlem medlem3 = new Medlem
+            {
+                MedlemsNummer = 33445,
+                Fornavn = "Bobsine",
+                Efternavn = "Bobsson",
+                Adresselinie1 = "Gamlegade 321",
+                Adresselinie2 = "st.th.",
+                PostNr = "5000",
+                ByNavn = "Odense C",
+                MailAdresse = "bobsine@thebob.dk"
+            };
+
+            Medlem medlem4 = new Medlem
+            {
+                MedlemsNummer = 54321,
+                Fornavn = "Jim",
+                Efternavn = "Doe",
+                Adresselinie1 = "Langevej 456",
+                PostNr = "8000",
+                ByNavn = "Aarhus C",
+                MailAdresse = "jim@doe.dk",
+                ErTilmeldtNyhedsbrev = true,
+                DatoBrev = "18. august 2023"
+            };
+
+            Medlem medlem5 = new Medlem
+            {
+                MedlemsNummer = 56789,
+                Fornavn = "Jimbob",
+                Efternavn = "Bobsson",
+                Adresselinie1 = "Kortevej 3",
+                Adresselinie2 = "st.tv.",
+                PostNr = "8200",
+                ByNavn = "Aarhus N",
+                MailAdresse = "jimbob@bobjim.dk"
+            };
+
+            medlemmer.Add(medlem1);
+            medlemmer.Add(medlem2);
+            medlemmer.Add(medlem3);
+            medlemmer.Add(medlem4);
+            medlemmer.Add(medlem5);
+        }
+    }
+}
---
+++ EksamensprojektOpg7/Translator.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace EksamensprojektOpg7
+{
+    internal class Translator
+    {
+        protected MessageQueue InQueue;
+        protected MessageQueue OutQueue;
+
+        public Translator(MessageQueue inQueue, MessageQueue outQueue)
+        {
+            InQueue = inQueue;
+            OutQueue = outQueue;
+
+            InQueue.Formatter = new XmlMessageFormatter(new string[] { "System.String,mscorlib" });
+            InQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(OnMessage);
+            InQueue.BeginReceive();
+        }
+
+        public void OnMessage(Object source, ReceiveCompletedEventArgs asyncResult)
+        {
+            // modtager beskeden
+            MessageQueue mq = (MessageQueue)source;
+            Message message = mq.EndReceive(asyncResult.AsyncResult);
+
+            // read the content from the original message
+            string XMLDocument;
+            using (Stream body = message.BodyStream)
+            using (StreamReader reader = new StreamReader(body))
+            {
+                XMLDocument = reader.ReadToEnd();
+            }
+
+            // extracts the needed data
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(XMLDocument);
+            string nummer = xml.SelectSingleNode("Medlem/Medlemsnummer").InnerText;
+            string fornavn = xml.SelectSingleNode("Medlem/Fornavn").InnerText;
+            string efternavn = xml.SelectSingleNode("Medlem/Efternavn").InnerText;
+            string adresse = xml.SelectSingleNode("Medlem/Adresse").InnerText;
+            string by = xml.SelectSingleNode("Medlem/ByNavn").InnerText;
+            string dato = xml.SelectSingleNode("Medlem/DatoBrev").InnerText;
+
+            // translates the data
+            // create the XmlDocument object
+            XmlDocument xmlDocument = new XmlDocument();
+            // root element
+            XmlElement memberElement = xmlDocument.CreateElement("Member");
+            // children elements
+            XmlElement membershipNumberElement = xmlDocument.CreateElement("MembershipNumber");
+            membershipNumberElement.InnerText = nummer;
+            XmlElement nameElement = xmlDocument.CreateElement("Name");
+            nameElement.InnerText = fornavn + " " + efternavn;
+            XmlElement addressElement = xmlDocument.CreateElement("Address");
+            addressElement.InnerText = adresse + ",\n " + by;
+            XmlElement zipCodeElement = xmlDocument.CreateElement("ZipCode");
+            zipCodeElement.InnerText = xml.SelectSingleNode("Medlem/PostNr").InnerText;
+            XmlElement mailElement = xmlDocument.CreateElement("Mail");
+            mailElement.InnerText = xml.SelectSingleNode("Medlem/MailAdresse").InnerText;
+            XmlElement dateElement = xmlDocument.CreateElement("DateRegistered");
+            dateElement.InnerText = ConvertDanishToAmericanDate(dato);
+
+            // append children elements to the root element
+            memberElement.AppendChild(membershipNumberElement);
+            memberElement.AppendChild(nameElement);
+            memberElement.AppendChild(addressElement);
+            memberElement.AppendChild(zipCodeElement);
+            memberElement.AppendChild(mailElement);
+            memberElement.AppendChild(dateElement);
+            // append the root element to the XmlDocument
+            xmlDocument.AppendChild(memberElement);
+
+            // sends the translated message to the receiving channel
+            Message subscribeMessage = new Message
+            {
+                Body = xmlDocument,
+                Label = "New member for the Newsletter",
+                Formatter = new XmlMessageFormatter(new string[] { "System.String,mscorlib" })
+            };
+
+            // send to the out queue
+            try
+            {
+                OutQueue.Send(subscribeMessage);
+            }
+            catch (MessageQueueException e)
+            {
+                Console.WriteLine("Error sending message: " + e.Message);
+            }
+
+            // continue listening for messages
+            InQueue.BeginReceive();
+        }
+
+        public static string ConvertDanishToAmericanDate(string danishDate)
+        {
+            var danishCulture = new CultureInfo("da-DK");
+
+            // try to parse the Danish date string into a DateTime
+            if (DateTime.TryParseExact(danishDate, "dd. MMMM yyyy", danishCulture, DateTimeStyles.None, out DateTime parsedDate))
+            {
+                // format the successfully parsed DateTime as MM/dd/yyyy f.ex. 10/23/2024
+                return parsedDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                throw new FormatException($"The date '{danishDate}' is not in the expected format 'dd. MMMM yyyy'!");
+            }
+        }
+    }
+}
